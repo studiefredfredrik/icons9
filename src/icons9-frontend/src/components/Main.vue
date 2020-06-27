8b2546cc23b20450fa5096743e0e944bd70d5a1e
studiefredfredrik <studiefredfredrik@gmail.com>
2020-06-27 03:02:06 +0200

    wip

--- src/icons9-frontend/src/components/Main.vue
+++ src/icons9-frontend/src/components/Main.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" class="container">
+    <div class=""></div>
     <div class="heading">Icons9</div>
 
     <div class="search-container">
@@ -11,7 +12,8 @@
       <!--</div>-->
     <div v-if="icons && icons.length > 0" class="file-container">
       <div v-for="(file, index) in icons" class="list-item" :key="index">
-        <img class="svg" v-bind:src="file.path" v-bind:alt="file.path" @click="copyToClipboard(file)">
+<!--        <img class="svg" v-bind:src="file.path" v-bind:alt="file.path" @click="copyToClipboard(file)">-->
+        <img class="svg" v-bind:src="getImagePath(file)" v-bind:alt="file.path" @click="copyToClipboard(file)">
         <div class="full-name">{{file.fullName}}</div>
       </div>
     </div>
@@ -57,6 +59,17 @@ export default {
       copyText.value = file.image.replace('<svg', '<svg width="50px" height="50px"')
       copyText.select();
       document.execCommand("copy");
+    },
+    getImagePath(file){
+      let color = '#ff0011'
+      let imgext = encodeURIComponent(file.image.replace('<path ', `<path style="fill:${color};stroke:${color};" `))
+      console.log(imgext)
+
+      //imgext = encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 512"><path style="fill:green;stroke:green;" d="M205.9 512l31.1-38.4c12.3-.2 25.6-1.4 36.5-6.6 38.9-18.6 38.4-61.9 38.3-63.8-.1-5-.8-4.4-28.9-37.4H362c-.2 50.1-7.3 68.5-10.2 75.7-9.4 23.7-43.9 62.8-95.2 69.4-8.7 1.1-32.8 1.2-50.7 1.1zm200.4-167.7c38.6 0 58.5-13.6 73.7-30.9l-175.5-.3-17.4 31.3 119.2-.1zm-43.6-223.9v168.3h-73.5l-32.7 55.5H250c-52.3 0-58.1-56.5-58.3-58.9-1.2-13.2-21.3-11.6-20.1 1.8 1.4 15.8 8.8 40 26.4 57.1h-91c-25.5 0-110.8-26.8-107-114V16.9C0 .9 9.7.3 15 .1h82c.2 0 .3.1.5.1 4.3-.4 50.1-2.1 50.1 43.7 0 13.3 20.2 13.4 20.2 0 0-18.2-5.5-32.8-15.8-43.7h84.2c108.7-.4 126.5 79.4 126.5 120.2zm-132.5 56l64 29.3c13.3-45.5-42.2-71.7-64-29.3z"/></svg>`)
+      let res = `data:image/svg+xml;utf8,${imgext}`
+
+      // res = btoa(res)
+      return res
     }
   }
 }
@@ -102,8 +115,8 @@ export default {
     margin: auto;
     height: 30px;
     width: auto;
-    color: black;
     cursor: pointer;
+
   }
   .svg:active{
     color:red;
