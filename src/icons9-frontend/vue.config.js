ed43c1e1a287e2e0f08a068a76e872763477f1d3
studiefredfredrik <studiefredfredrik@gmail.com>
2020-06-27 05:26:31 +0200

    color and mdi icons

--- src/icons9-frontend/vue.config.js
+++ src/icons9-frontend/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: './',
   devServer: {
-    proxy: 'http://localhost:5050'
+    proxy: 'http://localhost:5000'
   }
 }
--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ svgToDataURL = svgStr => {
 	return dataUrl
 }
 
-glob(`./icons9-frontend/dist/svgs/**/*.svg`, null, async function (er, files) {
+glob(`./icons9-frontend/dist/svgs/fas/**/*.svg`, null, async function (er, files) {
 	let based = []
 	files.forEach(file => {
 		let data = fs.readFileSync(file)
@@ -32,17 +32,47 @@ glob(`./icons9-frontend/dist/svgs/**/*.svg`, null, async function (er, files) {
 		})
 	})
 
-	fs.writeFile('./library.json', JSON.stringify(based), function (err) {
+	fs.writeFile('./library-fas.json', JSON.stringify(based), function (err) {
 		if (err) {
 			console.log('Error writing library to file')
 		}
-		console.log('\x1b[36m%s\x1b[0m',`Library is now ready to be served`)
+		console.log('\x1b[36m%s\x1b[0m',`Library-fas is now ready to be served`)
+	});
+})
+
+glob(`./icons9-frontend/dist/svgs/mdi/**/*48px.svg`, null, async function (er, files) {
+	let based = []
+	files.forEach(file => {
+		let data = fs.readFileSync(file)
+		let path = file.replace('./icons9-frontend/dist/','')
+		let prefix = file.startsWith('brands') ? 'fab' : 'fas'
+		let name = 'mdi-' + path.substring(path.lastIndexOf('/')+1).replace('.svg','').replace('ic_','').replace('_48px','').replace(/_/g, '-')
+		based.push({
+			encoded: svgToDataURL(data.toString()),
+			image: data.toString(),
+			path: path,
+			prefix: prefix,
+			name: name,
+			fullName: `${name}`
+		})
+	})
+
+	fs.writeFile('./library-mdi.json', JSON.stringify(based), function (err) {
+		if (err) {
+			console.log('Error writing library to file')
+		}
+		console.log('\x1b[36m%s\x1b[0m',`Library-mdi is now ready to be served`)
 	});
 })
 
 app.use('/', express.static('icons9-frontend/dist'))
-app.get('/api/library', function (req, res) {
-	fs.readFile('./library.json', 'utf-8', function(err, buf) {
+app.get('/api/library-fas', function (req, res) {
+	fs.readFile('./library-fas.json', 'utf-8', function(err, buf) {
+		res.send(buf.toString());
+	});
+})
+app.get('/api/library-mdi', function (req, res) {
+	fs.readFile('./library-mdi.json', 'utf-8', function(err, buf) {
 		res.send(buf.toString());
 	});
 })
